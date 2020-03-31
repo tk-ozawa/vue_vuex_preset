bc1f5cd03c5f172ba2ee47eb238b240811ab6d70
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-03-31 17:41:50 +0900

    add: table

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <h1>チュートリアルTodoリスト</h1>
+
+    <table>
+      <thead v-pre>
+        <tr>
+          <th class="id">ID</th>
+          <th class="comment">コメント</th>
+          <th class="state">状態</th>
+          <th class="button">-</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr></tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
