f82bf5ddb1a398873d6bc0f46e2e09737268ac93
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-04-02 16:54:20 +0900

    update: state記述

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos: [],
+    id: 0,
+    options: [
+      { value: -1, label: 'すべて' },
+      { value: 0, label: '作業中' },
+      { value: 1, label: '完了' }
+    ],
+    currentOptions: -1
+  },
+  getters: {
+
   },
   mutations: {
   },
